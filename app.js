c8b2376a95706cdd6ad4d81e31e1730fb41a74f8
Ahmed Nawar <ahmednawar2007@gmail.com>
2021-05-17 14:10:24 +0200

    Fix navbar click and slide

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-    const body = document.querySelector('body');
 
 
     burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        body.classList.toggle('nav-active-body');
-
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-
-        burger.classList.toggle('toggle');
+        toggleNavbar();
 
     });
+    navLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            toggleNavbar();
+        })
+    })
 
+
+}
+const toggleNavbar = () => {
+    const burger = document.querySelector('.burger');
+    const body = document.querySelector('body');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+    nav.classList.toggle('nav-active');
+    body.classList.toggle('nav-active-body');
+
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+    });
+
+    burger.classList.toggle('toggle');
 }
 navSlide();
